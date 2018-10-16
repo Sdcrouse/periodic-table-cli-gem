8830a63050b3a5361ae14b26f08f48951aedb4bb
Steven Crouse <sdcrouse91@gmail.com>
2018-10-16 22:55:15 +0000

    Updated #call and added #start and #main_menu methods to cli.rb

--- lib/periodic_table/cli.rb
+++ lib/periodic_table/cli.rb
@@ -1,5 +1,26 @@
 class PeriodicTable::CLI 
   def call 
-    puts "Hello again, world!"
+    # call the scraper here
+    start
+    puts "\nUntil next time, future chemist!"
+  end
+  
+  def start
+    puts "Welcome to the Interactive Periodic Table!"
+    puts "Ready to start learning some chemistry? (Y/n)"
+    start_program = gets.strip.downcase
+    
+    main_menu unless start_program == "n" || start_program == "no"
+  end
+  
+  def main_menu
+    options = {1: "Quit"}
+    yes_or_no = "no"
+    user_choice = nil
+    
+    puts "Welcome to the Main Menu! Here are your options:"
+    puts "Option 1, Option 2, Option 3"
+    
+    puts "What would you like to do?"
   end
 end
