89a1904830c37dc71cadb248485d97d67a2d52d4
Steven Crouse <sdcrouse91@gmail.com>
2018-10-16 22:07:01 +0000

    Changed a comment in config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
 
-# Note: Be VERY careful about the order in which you call these files.
+# Be VERY careful about the order in which you call these files!
 require_relative "../lib/periodic_table/version"
 require_relative "../lib/periodic_table/cli"
