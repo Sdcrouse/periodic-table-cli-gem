95a08e0a5c0c3f48391b6e2212b72cfb83484d4a
Steven Crouse <sdcrouse91@gmail.com>
2018-10-18 23:39:27 +0000

    Update environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,8 @@
+require "pry"
+require "open-uri"
+require "nokogiri"
 
 # Be VERY careful about the order in which you call these files!
 require_relative "../lib/periodic_table/version"
 require_relative "../lib/periodic_table/cli"
+require_relative "../lib/periodic_table/elements"
