3b8c8c9077f42ee273726672cb6ab9d5fd631280
Steven Crouse <sdcrouse91@gmail.com>
2018-10-19 00:26:07 +0000

    Stub #Element.new_from_table

--- lib/periodic_table/elements.rb
+++ lib/periodic_table/elements.rb
@@ -1,14 +1,32 @@
 class PeriodicTable::Element 
-  attr_accessor :name, :symbol, :atomic_number, :atomic_weight, :name_origin, :group, :period, :density, :melting_point, :boiling_point, :heat_capacity, :electronegativity, :abundance 
+  attr_accessor :name, :symbol, :atomic_number, :atomic_weight, :element_url, :name_origin, :group, :period, :density, :melting_point, :boiling_point, :heat_capacity, :electronegativity, :abundance 
   
   @@all = [] 
   
-  def initialize 
+  def initialize(attributes) 
+    attributes.each{|key, value| self.send("#{key}=", value)}
     @@all << self 
   end 
   
-  def self.new_from_table(table)
-    
+  def self.new_from_table(table = nil) # Create a new Element from the scraped periodic table.
+    # Note: unless I want to explain uncertainty, I may need to gsub the @atomic_weight values' parentheses with ""
+    attributes = {
+      name: "Hydrogen",
+      symbol: "H",
+      atomic_number: 1,
+      atomic_weight: 1.008,
+      element_url: "https://en.wikipedia.org/wiki/Hydrogen",
+      name_origin: "composed of the Greek elements hydro- and -gen meaning 'water-forming'",
+      group: 1,
+      period: 1,
+      density: 0.00008988,
+      melting_point: 14.01,
+      boiling_point: 20.28,
+      heat_capacity: 14.304,
+      electronegativity: 2.20,
+      abundance: 1400
+    }
+    self.new(attributes)
   end
   
    def self.all 
