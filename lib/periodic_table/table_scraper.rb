e4cfd60ef268369594c96db9818c9194306ac5cd
Steven Crouse <sdcrouse91@gmail.com>
2018-10-20 01:06:30 +0000

    Add new methods to TableScraper class and stub them

--- lib/periodic_table/table_scraper.rb
+++ lib/periodic_table/table_scraper.rb
@@ -1,4 +1,23 @@
 class PeriodicTable::TableScraper
 
+  def scrape_and_create_elements
+    page = self.get_page("https://en.wikipedia.org/wiki/List_of_chemical_elements")
+    #scraped_elements = self.scrape_elements_from(page)
+    #self.make_elements_from(scraped_elements)
+  end
+
+  def get_page(page)
+    "I got the page!"
+  end
+
+  def scrape_elements_from(page)
+    "I scraped the elements!"
+  end
+
+  def make_elements_from(scraped_elements)
+    puts "I made the elements!"
+  end
 
 end
+
+PeriodicTable::TableScraper.new.scrape_and_create_elements
