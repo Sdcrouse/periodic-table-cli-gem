706e3b6e501e0ec3e8bc98fad2bf18bf3030afd4
Steven Crouse <sdcrouse91@gmail.com>
2018-10-20 08:38:27 +0000

    Modify #scrape_and_create_elements, modify #make_properties_hash_from

--- lib/periodic_table/table_scraper.rb
+++ lib/periodic_table/table_scraper.rb
@@ -3,8 +3,10 @@ class PeriodicTable::TableScraper
   def scrape_and_create_elements
     page = self.get_page("https://en.wikipedia.org/wiki/List_of_chemical_elements")
     scraped_elements = self.scrape_elements_from(page)
+    scraped_elements.delete(scraped_elements.first) #Somehow, Nokogiri made the first node of scraped_elements a tr node from thead instead of tbody.
     scraped_elements.delete(scraped_elements.last) #Remove the last node, which contains notes and no chemical elements.
-    elements_with_properties = scraped_elements.collect {|element| self.get_properties_of(element)}
+    self.make_properties_hash_from(scraped_elements[0]) #Delete this line and uncomment the line below.
+    #elements_with_properties = scraped_elements.collect {|element| self.make_properties_hash_from(element)}
     #Create new Element instances here!
     #binding.pry
   end
@@ -20,7 +22,7 @@ class PeriodicTable::TableScraper
 
   def make_properties_hash_from(scraped_element)
     element_properties_hash = {}
-
+    #Why does scraped_element.css("td") keep returning [] ?
     binding.pry
 
   end
