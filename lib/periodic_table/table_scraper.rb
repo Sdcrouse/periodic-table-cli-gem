b799567892668b17dc859edaea2a2a2866ef63c3
Steven Crouse <sdcrouse91@gmail.com>
2018-10-23 23:54:43 +0000

    Refactor #element_type_from method

--- lib/periodic_table/table_scraper.rb
+++ lib/periodic_table/table_scraper.rb
@@ -83,28 +83,6 @@ class PeriodicTable::TableScraper
     else 
       "Error. Cannot determine the element type from the background color."
     end
-    case background_color
-    when "#f0ff8f"
-      "Reactive nonmetal"
-    when "#c0ffff"
-      "Noble gas"
-    when "#ffdead"
-      "Alkaline earth metal"
-    when "#cccc99"
-      "Metalloid"
-    when "#cccccc"
-      "Post-transition metal"
-    when "#ffc0c0"
-      "Transition metal"
-    when "#ffbfff"
-      "Lanthanide"
-    when "#ff99cc"
-      "Actinide"
-    when "#e8e8e8"
-      "Unknown chemical properties"
-    else 
-      "Error. Cannot determine the element type from the background color."
-    end
   end
 end
 
