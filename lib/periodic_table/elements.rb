f9a0af239e87e2e3687adf898fad5b803b222500
Steven Crouse <sdcrouse91@gmail.com>
2018-10-27 02:39:44 +0000

    Refactor the method #new_from_periodic_table
    Delete unnecessary comments. Then, clarify the purpose of the Element class method #new_from_periodic_table by changing its argument's name from table to element_attributes.

--- lib/periodic_table/elements.rb
+++ lib/periodic_table/elements.rb
@@ -7,27 +7,11 @@ class PeriodicTable::Element
     @@all << self
   end
 
-  def self.new_from_periodic_table(table = {}) # Create a new Element from the scraped periodic table.
-    # Note: unless I want to explain uncertainty, I may need to gsub the @atomic_weight values' parentheses with ""
-    #attributes = {
-    #  atomic_number: 1,
-    #  symbol: "H",
-    #  element_type: "Reactive nonmetal",
-    #  name: "Hydrogen",
-    #  element_url: "https://en.wikipedia.org/wiki/Hydrogen",
-    #  name_origin: "composed of the Greek elements hydro- and -gen meaning 'water#-forming'",
-    #  group: 1,
-    #  period: 1,
-    #  atomic_weight: 1.008,
-    #  density: 0.00008988,
-    #  melting_point: 14.01,
-    #  boiling_point: 20.28,
-    #  heat_capacity: 14.304,
-    #  electronegativity: 2.20,
-    #  abundance: 1400
-    #}
+  def self.new_from_periodic_table(element_attributes = {}) 
+    # Create a new Element from the scraped periodic table.
+    
     self.new.tap do |element| 
-      table.each{|key, value| element.send("#{key}=", value)}
+      element_attributes.each{|key, value| element.send("#{key}=", value)}
     end
   end
 
