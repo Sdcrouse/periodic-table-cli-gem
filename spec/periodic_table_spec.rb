4cb7bc2573e95c2f7e77b94adb1594d956b31caf
Steven Crouse <sdcrouse91@gmail.com>
2018-10-30 23:56:43 +0000

    Change test to reflect the main project goal

--- spec/periodic_table_spec.rb
+++ spec/periodic_table_spec.rb
@@ -3,9 +3,10 @@ RSpec.describe PeriodicTable do
     expect(PeriodicTable::VERSION).not_to be nil
   end
 
-  it "scrapes the periodic table and makes an array of chemical elements" do
+  it "scrapes the periodic table and creates new Elements" do
     elements_array = PeriodicTable::TableScraper.new.scrape_periodic_table
     
     expect(PeriodicTable::CLI.new.make_elements).to eq(elements_array)
+    expect(PeriodicTable::Element.all.size).to eq(118)
   end
 end
