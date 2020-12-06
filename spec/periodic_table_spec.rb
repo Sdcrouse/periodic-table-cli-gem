47b86aef583feb2d8f567e6414a057709c75eaff
sdcro <sdcrouse91@gmail.com>
2020-12-05 17:01:11 -0800

    Split test into two parts: one for scraping, and one for Element creation

--- spec/periodic_table_spec.rb
+++ spec/periodic_table_spec.rb
@@ -3,10 +3,15 @@ RSpec.describe PeriodicTable do
     expect(PeriodicTable::VERSION).not_to be nil
   end
 
-  it "scrapes the periodic table and creates new Elements" do
+  it "scrapes all of the elements from the periodic table" do
     elements_array = PeriodicTable::TableScraper.scrape_periodic_table
     
-    expect(PeriodicTable::CLI.new.make_elements).to eq(elements_array)
+    expect(elements_array.size).to eq(118)
+  end
+
+  it "creates new Elements from the scraped data" do
+    PeriodicTable::CLI.new.make_elements
+
     expect(PeriodicTable::Element.all.size).to eq(118)
   end
 end
