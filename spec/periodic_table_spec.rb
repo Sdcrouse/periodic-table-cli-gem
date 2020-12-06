7ab94a1aead0a9d3a39b4530a4b123380af507c6
sdcro <sdcrouse91@gmail.com>
2020-12-05 17:30:12 -0800

    Add test that checks that the CLI saves the correct information for an Element

--- spec/periodic_table_spec.rb
+++ spec/periodic_table_spec.rb
@@ -14,4 +14,27 @@ RSpec.describe PeriodicTable do
 
     expect(PeriodicTable::Element.all.size).to eq(118)
   end
+
+  it "saves the correct information for an Element" do
+    # This test may need to be updated when Wikipedia's Periodic Table is updated.
+
+    PeriodicTable::CLI.new.make_elements
+
+    boron = PeriodicTable::Element.all[4]
+
+    expect(boron.atomic_number).to eq("5")
+    expect(boron.symbol).to eq("B")
+    expect(boron.name).to eq("Boron")
+    expect(boron.element_type).to eq("Metalloid")
+    expect(boron.name_origin).to eq("Borax, a mineral (from Arabic bawraq)")
+    expect(boron.group).to eq("13")
+    expect(boron.period).to eq("2")
+    expect(boron.atomic_weight).to eq(10.81)
+    expect(boron.melting_point).to eq("2349")
+    expect(boron.boiling_point).to eq("4200")
+    expect(boron.heat_capacity).to eq("1.026")
+    expect(boron.electronegativity).to eq("2.04")
+    expect(boron.abundance).to eq("10")
+    expect(boron.element_url).to eq("https://en.wikipedia.org/wiki/Boron")
+  end
 end
